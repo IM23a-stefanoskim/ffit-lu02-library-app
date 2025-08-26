aa9d90c03143f1383deac09f869ee54e17d06b06
IM23a-stefanoskim <stefanoskim@BZZ.ch>
2025-08-26 11:18:02 +0200

    Book.java with necessary code created  for expanded code in LibraryAppMain.java to get Books title.

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 public class LibraryAppMain {
 
+    private static final Book BOOK_1 = new Book(
+            1,
+            "978-3-8362-9544-4",
+            "Java ist auch eine Insel",
+            "Christian Ullenboom",
+            2023
+    );
+
+    private static final Book BOOK_2 = new Book(
+            2,
+            "978-3-658-43573-8",
+            "Grundkurs Java",
+            "Dietmar Abts",
+            2024
+    );
+
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
         String command = "";
+
+
 
         while (!command.equals("quit")) {
             System.out.println("Enter command: ");
@@ -17,11 +35,20 @@ public class LibraryAppMain {
                     System.out.println("help");
                     break;
                 case "hello":
-                    System.out.println("hello");
+                    System.out.println("hola sexy");
+                    break;
+                case "listBooks":
+                    listBooks();
+                    break;
                 default:
                     System.out.println("Ungültige Eingabe: '" + command + "'");
                     break;
             }
         }
+
+    }
+    private static void listBooks() {
+        System.out.println(BOOK_1.getTitle());
+        System.out.println(BOOK_2.getTitle());
     }
 }
