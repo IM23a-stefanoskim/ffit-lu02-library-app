f4d8b7cff55e431b4bfd6e3346e734b109e73b78
IM23a-stefanoskim <stefanoskim@BZZ.ch>
2025-08-26 10:36:59 +0200

    LibraryAppMain implemented and all tests passed.

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -7,7 +7,6 @@ public class LibraryAppMain {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-
         String command = "";
 
         while (!command.equals("quit")) {
