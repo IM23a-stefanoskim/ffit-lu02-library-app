0f1b441487e3ab0bf3f49ad0b341ef540f828112
IM23a-stefanoskim <stefanoskim@BZZ.ch>
2025-08-26 10:31:44 +0200

    LibraryAppMain implemented and all tests passed.

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,28 @@
 package ch.bzz;
 
+import java.util.Scanner;
+
 public class LibraryAppMain {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        Scanner myScanner = new Scanner(System.in);
+
+
+        String command = "";
+
+        while (!command.equals("quit")) {
+            System.out.println("Enter command: ");
+            command = myScanner.nextLine();
+            switch (command){
+                case "help":
+                    System.out.println("help");
+                    break;
+                case "hello":
+                    System.out.println("hello");
+                default:
+                    System.out.println("Ungültige Eingabe: '" + command + "'");
+                    break;
+            }
+        }
     }
 }
